a286f290093635f56a87b70ab82c9b907bff89d8
Vinit2000 <vinitonmalwar20@gmail.com>
2025-05-17 11:57:18 +0530

    Server up and running

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = required("express");
-const cors = required("cors");
+const express = require("express");
+const cors = require("cors");
 const connectToDatabase = require('./config/db')
 const insuranceRoutes = require('./routes/insuranceRoutes');
 
@@ -9,3 +9,10 @@ const PORT = 5000;
 const allowedOrigins = [
     'https://localhost:5173'  //frontend deployed link should be pasted here
 ]
+
+// DB connection
+connectToDatabase();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
