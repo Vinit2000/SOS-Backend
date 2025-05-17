81343147e952e427fa7195f3ba26b9065927eddd
Vinit2000 <vinitonmalwar20@gmail.com>
2025-05-17 13:28:02 +0530

    Schema Updated

--- models/insuranceModels.js
+++ models/insuranceModels.js
@@ -6,3 +6,44 @@
 5. Representative Name 
 6. Email
 7. Password*/
+
+const mongoose = require('mongoose');
+
+const patientSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        match:/^[\w\s]+$/
+    },
+    email:{
+        type: String,
+        required:true,
+        unique:true,
+        match: /^[\w.-]+@([\w-]+\.)+[\w-]{2,4}$/
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:6
+    },
+    patientId:{
+        type: String,
+        required:true,
+        unique:true
+    },
+    representativeName:{
+        type:String,
+        match:/^[A-Za-z0-9\s]+$/
+    },
+    reference:{
+        type:String
+    },
+    phoneNumber:{
+        type:String,
+        match:/[0-9]{10}$/
+    },
+})
+
+const Patient = mongoose.model('Patient', patientSchema ,'insuranceforms' )
+
+module.exports = Patient
